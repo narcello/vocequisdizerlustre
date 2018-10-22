4090da7e9c64a45e11547d1f9873b727161b8c65
MarcelloVSilva <marcellovcs@gmail.com>
2018-10-22 15:10:27 -0200

    Updates

---
+++ precache-manifest.7910a741b16a291d7ede6e9a8d0eb14f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff27f97d9f3cbcf11fec300fb746a561",
+    "url": "/vocequisdizerlustre/static/media/lutreEpCapa_n.ff27f97d.jpg"
+  },
+  {
+    "revision": "35a0488c178b27707920d00ca6ca704b",
+    "url": "/vocequisdizerlustre/static/media/capaApego.35a0488c.jpg"
+  },
+  {
+    "revision": "3f80f30ded8eb7d19474f20ab271664c",
+    "url": "/vocequisdizerlustre/static/media/capa.3f80f30d.jpg"
+  },
+  {
+    "revision": "0c48c534e8b73c09dac8e6768307d44f",
+    "url": "/vocequisdizerlustre/static/media/aoVivoCCUFG_cortada.0c48c534.jpg"
+  },
+  {
+    "revision": "f572157511f75e3e7276",
+    "url": "/vocequisdizerlustre/static/js/runtime~main.f5721575.js"
+  },
+  {
+    "revision": "08324afc12865f56738b",
+    "url": "/vocequisdizerlustre/static/js/main.08324afc.chunk.js"
+  },
+  {
+    "revision": "fa4a3dc3b43592c2d504",
+    "url": "/vocequisdizerlustre/static/js/1.fa4a3dc3.chunk.js"
+  },
+  {
+    "revision": "08324afc12865f56738b",
+    "url": "/vocequisdizerlustre/static/css/main.72845ee4.chunk.css"
+  },
+  {
+    "revision": "51436c45255dc21636b56e2bcacff000",
+    "url": "/vocequisdizerlustre/index.html"
+  }
+];
