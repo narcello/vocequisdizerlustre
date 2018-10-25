6f6d027c82a52a46b3529ca551df2b478163f589
MarcelloVSilva <marcellovcs@gmail.com>
2018-10-25 19:37:32 -0200

    refactor and add marker in map linked with register in firebase

--- src/components/mapa/Mapa.js
+++ src/components/mapa/Mapa.js
@@ -4,53 +4,89 @@ import loadGoogleMapsApi from 'load-google-maps-api'
 import apiKey from './index'
 import './Mapa.css'
 
-class RenderMapa extends Component {
+export default class RenderMapa extends Component {
     constructor() {
         super()
         this.state = {
-            teste: null
+            coordinates: [null],
+            myLatLng: { lat: -16.6806141, lng: -49.2585262 }
         }
-    }
-    componentDidMount() {
-        const options = {
+        this.options = {
             key: apiKey
         }
+        this.map = null;
+        this.criaMarcadoresNoMapa = this.criaMarcadoresNoMapa.bind(this)
+        this.addMarcadorNoBanco = this.addMarcadorNoBanco.bind(this)
+        this.pegaLocalizacaoDoBrowser = this.pegaLocalizacaoDoBrowser.bind(this)
+    }
+    componentWillMount() {
+        const self = this
+        this.pegaLocalizacaoDoBrowser()
 
-        loadGoogleMapsApi(options).then(function (googleMaps) {
-            new googleMaps.Map(document.querySelector('#map'), {
-                center: {
-                    lat: 40.7484405,
-                    lng: -73.9944191
-                },
-                zoom: 12
+        let coordinates = []
+        firebase.database().ref('geoLocations').once('value', (snapshot) => {
+            Object.keys(snapshot.val()).map((key) =>
+                coordinates.push(snapshot.val()[key].coordinates))
+        }).then(() => this.setState({ coordinates })
+        ).then(() => this.criaMarcadoresNoMapa())
+
+        loadGoogleMapsApi(this.options).then(function (googleMaps) {
+            self.map = new googleMaps.Map(document.querySelector('#map'), {
+                center: self.state.myLatLng,
+                zoom: 9,
             })
         }).catch(function (error) {
             console.error(error)
         })
     }
-    render() {
-        return (<div id='map'></div>)
-    }
-}
-
-export default class Mapa extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            teste: null
+    pegaLocalizacaoDoBrowser = () => {
+        let myLatLng = { lat: null, lng: null };
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                myLatLng.lat = position.coords.latitude
+                myLatLng.lng = position.coords.longitude
+                this.setState({ myLatLng })
+            })
         }
     }
-    componentDidMount() {
-        firebase.database().ref('geoLocations').on('value', (snapshot) => {
-            this.setState({ teste: snapshot.val() });
-        });
+    criaMarcadoresNoMapa = () => {
+        const self = this
+        this.state.coordinates.map((cordenada) => {
+            return loadGoogleMapsApi(this.options).then((googleMaps) => {
+                new googleMaps.Marker({
+                    position: cordenada,
+                    map: self.map,
+                    title: 'Hi'
+
+                })
+            }).catch(function (error) {
+                console.error(error)
+            })
+        })
+    }
+    addMarcadorNoBanco = () => {
+        const self = this
+        const myCoordinates = {
+            lat: this.state.myLatLng.lat,
+            lng: this.state.myLatLng.lng
+        }
+        const newPostKey = firebase.database().ref('geoLocations').push().key;
+        firebase.database().ref('geoLocations/' + newPostKey).set({
+            email: 'email',
+            coordinates: myCoordinates
+        }).then(() => {
+            self.state.coordinates.push(myCoordinates)
+            self.criaMarcadoresNoMapa()
+        })
     }
     render() {
         return (
-                <div id='container'>
-                    <div id='ladoEsquerdo'>{this.state.teste}</div>
-                    <div id='renderMapa'><RenderMapa /></div>
+            <div id='container'>
+                <div id='ladoEsquerdo'>
+                    <button onClick={this.addMarcadorNoBanco}>Venham tocar aqui</button>
                 </div>
+                <div id='map'></div>
+            </div>
         )
     }
 }
